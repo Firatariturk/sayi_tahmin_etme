a1c0f0d63d19354ecee66fe1f89383c5889f906d
aorus <aorus@DESKTOP-PF0OTUT>
2023-11-21 21:03:23 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+Random rnd = new Random();
+int sayi = rnd.Next(1, 101);
+int tahmin = 101;
+//Console.WriteLine(sayi);
+Console.WriteLine("bir sayi girin");
+while (sayi != tahmin)
+{
+    tahmin = Convert.ToInt32(Console.ReadLine());
+    if (sayi == tahmin) { Console.WriteLine("tebrikler sayi " + sayi +"'idi."); }
+    else
+    {
+        if (tahmin < sayi)
+        {
+            if (sayi - tahmin < 6)
+            {
+                Console.WriteLine("cok sicak");
+            }
+            else if (sayi - tahmin < 11)
+            {
+                Console.WriteLine("sicak");
+            }
+            else if (sayi - tahmin < 21)
+            {
+                Console.WriteLine("soguk");
+            }
+            else if (sayi - tahmin > 20)
+            {
+                Console.WriteLine("cok soguk");
+            }
+        }
+        else
+        {
+            if ((sayi - tahmin) * -1 < 6)
+            {
+                Console.WriteLine("cok sicak");
+            }
+            else if ((sayi - tahmin) * -1 < 11)
+            {
+                Console.WriteLine("sicak");
+            }
+            else if ((sayi - tahmin) * -1 < 21)
+            {
+                Console.WriteLine("soguk");
+            }
+            else if ((sayi - tahmin) * -1 > 20)
+            {
+                Console.WriteLine("cok soguk");
+            }
+        }
+    }
+}
